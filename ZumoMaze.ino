a654f08c7e7933e939de5c566100a233d0f88f3e
Ryan Hartley <rhartley@newdawn.com>
2015-03-25 23:01:15 -0600

    Initial commit

---
+++ ZumoMaze.ino
@@ -0,0 +1,91 @@
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+#include <ZumoMotors.h>
+#include <ZumoBuzzer.h>
+#include <Pushbutton.h>
+
+#define SENSOR_THRESHOLD 300
+#define ABOVE_LINE(sensor)((sensor) > SENSOR_THRESHOLD)
+#define TURN_SPEED 200
+#define SPEED 200
+#define LINE_THICKNESS .75
+#define INCHES_TO_ZUNITS 17142.0
+#define OVERSHOOT(line_thickness)(((INCHES_TO_ZUNITS * (line_thickness)) / SPEED))
+
+ZumoBuzzer buzzer;
+ZumoReflectanceSensorArray sensor;
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON);
+
+void setup()
+{
+  buzzer.play(">g32>>c32");
+  sensor.init();
+  button.waitForButton();
+  calibrate();
+}
+
+void loop()
+{
+  button.waitForButton();
+  //Start here
+  followLine();
+}
+
+void followLine()
+{
+  unsigned int position;
+  unsigned int sensors[6];
+  int offsetFromCenter;
+  int powerDifference;
+ 
+  while(1)
+  {
+    position = sensor.readLine(sensors);
+    offsetFromCenter = ((int)position) - 2500;
+    // powerDifference determines how sharp the turn is
+    powerDifference = offsetFromCenter / 3;
+    
+    if (powerDifference > SPEED)
+    {
+      powerDifference = SPEED;
+    }
+    if (powerDifference < -SPEED)
+    {
+      powerDifference = -SPEED;
+    }
+    
+    if (powerDifference < 0)
+    {
+      motors.setSpeeds(SPEED + powerDifference, SPEED);
+    }
+    else
+    {
+      motors.setSpeeds(SPEED, SPEED - powerDifference);
+    }
+    //Need to return when line lost or intersection
+  } 
+}
+
+void calibrate()
+{
+  delay(1000);
+  int i;
+  for(i = 0; i < 80; i++)
+  {
+    if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
+    {
+      motors.setSpeeds(SPEED/2, SPEED/2);
+    }
+    else
+    {
+      motors.setSpeeds(-SPEED/2, -SPEED/2);
+    }
+    sensor.calibrate();
+    delay(20);
+  }
+  
+  motors.setSpeeds(0, 0);
+  buzzer.play(">g32>>c32");
+}
+
